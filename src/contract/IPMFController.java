b569c52116a17b1518184273f280e1f6747c040e
FlorianVouters <florian.vouters@viacesi.fr>
2017-11-22 09:52:28 +0100

    Update Controller interface

--- src/contract/IPMFController.java
+++ src/contract/IPMFController.java
@@ -1,9 +1,11 @@
 package contract;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
+import java.util.Observer;
 
-public interface IPMFController {
+public interface IPMFController extends Runnable, Observer, ActionListener {
 
 	public void actionPerformed(ActionEvent e);
 	
