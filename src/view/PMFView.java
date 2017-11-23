26b5810af202a156aed5929c35a787d2a80ba46a
Robin Maisano <robin.maisano@gmail.com>
2017-11-23 13:53:24 +0100

    View import update

--- src/view/PMFView.java
+++ src/view/PMFView.java
@@ -1,20 +1,15 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import contract.IPMFView;
-
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.Color;
 
 public class PMFView extends JFrame implements IPMFView{
 
